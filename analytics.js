6f750478ed2bd54c7d89e8b804d67cc4ca31b2a7
Sparky <Sparkbot207@gmail.com>
2026-02-12 09:10:06 -0500

    Update analytics to use Tailscale endpoint
    - Changed from expired Cloudflare tunnel to Tailscale IP
    - Now tracks to Sparky's Pi analytics server

--- analytics.js
+++ analytics.js
@@ -1,5 +1,5 @@
 (function(){
-var E='https://adjustment-fundamentals-notification-copyrights.trycloudflare.com/api/t';
+var E='http://100.118.254.92:5001/api/t';
 var s=sessionStorage.getItem('_sid')||(sessionStorage._sid='s'+Date.now(),sessionStorage._sid);
 function t(type,el){new Image().src=E+'?s='+s+'&t='+type+'&p='+encodeURIComponent(location.pathname)+(el?'&e='+encodeURIComponent(el):'')+'&_='+Date.now();}
 t('pageview');
